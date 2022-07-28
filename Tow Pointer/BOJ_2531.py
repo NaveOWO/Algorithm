22af8204096709dce6345c677b2983ae89829afc
JY <pianowell96@gmail.com>
2022-07-28 17:54:02 +0900

    22.07.28

--- Tow Pointer/BOJ_2531.py
+++ Tow Pointer/BOJ_2531.py
@@ -1,4 +1,5 @@
 from collections import deque
+import collections
 
 n, d, k, c = map(int, input().split())
 cnt = 0
@@ -10,8 +11,27 @@ for i in range(n):
 for i in range(k-1):
     fishBelt.append(fishBelt[i])
 
+colaboDict = collections.defaultdict(int)
+for fish in range(1,d+1):
+    colaboDict[fish]
+
 totalDish = len(fishBelt)
 fishCollaboration = deque(fishBelt[0:k])
+
+for fish in fishCollaboration:
+    colaboDict[fish] += 1
+
 for i in range(k,totalDish):
+    tempCnt = 0
+    colaboDict[fishCollaboration[0]] -= 1
     fishCollaboration.popleft()
     fishCollaboration.append(fishBelt[i])
+    colaboDict[fishBelt[i]] += 1
+    for key in colaboDict:
+        if colaboDict[key] > 0:
+            tempCnt += 1
+    if colaboDict[c] == 0 :
+        tempCnt += 1
+    cnt = max(cnt, tempCnt)
+
+print(cnt)
