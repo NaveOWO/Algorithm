880778e039c9033d5e680e3d83849083e15a1a2a
NaveOWO <pianowell96@gmail.com>
2022-08-10 03:18:49 +0900

    스타트와 링크

--- Bruteforce/BOJ_14889.py
+++ Bruteforce/BOJ_14889.py
@@ -3,16 +3,29 @@ N = int(input())
 
 member = [i for i in range(1,N+1)]
 status = []
+interval = 99999999
 for i in range(N):
     status.append(list(map(int, input().split())))
 
 memberCombi = list(combinations(member, N//2))
-print(memberCombi)
+
+def getScoreSum(tempList):
+    scoreSum = 0
+    for combi in tempList:
+        r = combi[0]-1
+        c = combi[1]-1
+        scoreSum += status[r][c] + status[c][r]
+    return scoreSum
 
 for i in range(len(memberCombi)//2):
-    temp = [memberCombi[i],memberCombi[len(memberCombi)-i-1]]
-    print(temp)
-# for combi in memberCombi:
-#     start = combi[0:N//2]
-#     link = combi[N//2:]
-#     print(start, link)
+    tempTeam = [memberCombi[i],memberCombi[len(memberCombi)-i-1]]
+    startScoreCombi = list(combinations(tempTeam[0], 2))
+    startSum = getScoreSum(startScoreCombi)
+    linkScoreCombi = list(combinations(tempTeam[1], 2))
+    linkSum = getScoreSum(linkScoreCombi)
+    interval = min(interval, (abs(startSum - linkSum)))
+
+print(interval)
+
+
+
