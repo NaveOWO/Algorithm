e56c4a145ded39bd1690675f6c7a45b23ff9e05c
JY <pianowell96@gmail.com>
2022-08-12 15:55:31 +0900

    치킨배달

--- BFS/BOJ_15686.py
+++ BFS/BOJ_15686.py
@@ -4,52 +4,61 @@ import copy
 
 N, M = map(int, input().split())
 city = []
-chicken = []
+chickenList = []
+CHICKEN = 2
+HOME = 1
+
 for i in range(N):
     city.append(list(map(int, input().split())))
     for j in range(len(city[i])):
-        if city[i][j] == 2:
-            chicken.append((i,j))
-chickenCombi = list(combinations(chicken, M))
-for i in range(len(chickenCombi)):
-    chickenCombi[i] = deque(chickenCombi[i])
+        if city[i][j] == CHICKEN:
+            chickenList.append((i,j))
 
-for i in range(N):
-    for j in range(N):
-        if city[i][j] == 2:
-            city[i][j] = 0
+chickenCombiList = list(combinations(chickenList, M))
 
-def removeChicken(combi, tempCity):
+for i in range(len(chickenCombiList)):
+    chickenCombiList[i] = deque(chickenCombiList[i])
+
+def removeChicken(tempCity, chickenCombi):
     for r in range(N):
         for c in range(N):
-            if tempCity[r][c] == 2 and (r,c) not in combi:
-                tempCity[r][c] == 0
+            if tempCity[r][c] == CHICKEN and (r,c) not in chickenCombi:
+                tempCity[r][c] = 0
     return tempCity
 
 dr = [-1,0,1,0]
 dc = [0,1,0,-1]
-def getCityDistance(C,tempCity):
+def getCityDistance(chickenCombi, tempCity):
     cityDistance = 0
-    print(C)
-    while C:
-        r,c = C.popleft()
+    visit = [[0 for _ in range(N)] for i in range(N)]
+    while chickenCombi:
+        r,c = chickenCombi.popleft()
+        visit[r][c] = 1
         for i in range(4):
             nr = r + dr[i]
             nc = c + dc[i]
-            if nr < 0 or nr >= N or nc < 0 or nc >= N :
+            if nr < 0 or nr >= N or nc < 0 or nc >= N or visit[nr][nc] == 1:
                 continue
             if tempCity[nr][nc] == 0:
-                C.append((nr,nc))
-                tempCity[nr][nc] += tempCity[r][c]
-            elif tempCity[nr][nc] == 1:
-                tempCity[nr][nc] += tempCity[r][c]
-                cityDistance += tempCity[nr][nc]
+                chickenCombi.append((nr,nc))
+                tempCity[nr][nc] = tempCity[r][c] + 1
+                continue
+            if tempCity[nr][nc] == HOME:
+                chickenCombi.append((nr,nc))
+                tempCity[nr][nc] = tempCity[r][c] + 1
+                cityDistance += tempCity[nr][nc]-2
     return cityDistance
 
+
+
+
 resultList = []
-cnt = 0
-for i in range(len(chickenCombi)):
-    tmpCity = copy.deepcopy(city)
-    tmpCity = removeChicken(chickenCombi[i], tmpCity)
-    resultList.append(getCityDistance(chickenCombi[i],tmpCity))
-    cnt += 1
+for i in range(len(chickenCombiList)):
+    tempCity = copy.deepcopy(city)
+    tempCity = removeChicken(tempCity, chickenCombiList[i])
+    resultList.append(getCityDistance(chickenCombiList[i], tempCity))
+
+print(min(resultList))
+
+
+
