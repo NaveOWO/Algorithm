78b6f0245a968035b284d6461e0221598a864318
NaveOWO <pianowell96@gmail.com>
2022-09-30 16:47:41 +0900

    순위검색

--- Programmers/2021 kakao blind_순위검색.L2.py
+++ Programmers/2021 kakao blind_순위검색.L2.py
@@ -1,61 +1,65 @@
 def solution(info, query):
-    result = [0 for i in range(len(query))]
+    APPLICANTS = len(info)
+    CONDITIONS = len(query)
 
-    # info 분리하기
-    infoDic = {'language': [], 'category': [], 'history': [], 'food': [], 'score': []}
+    dictCombi = {"cpp": {"backend": {"junior": {"pizza": {}, "chicken": {}}, "senior": {"pizza": {}, "chicken": {}}},
+                         "frontend": {"junior": {"pizza": {}, "chicken": {}}, "senior": {"pizza": {}, "chicken": {}}}},
+                 "java": {"backend": {"junior": {"pizza": {}, "chicken": {}}, "senior": {"pizza": {}, "chicken": {}}},
+                          "frontend": {"junior": {"pizza": {}, "chicken": {}}, "senior": {"pizza": {}, "chicken": {}}}},
+                 "python": {"backend": {"junior": {"pizza": {}, "chicken": {}}, "senior": {"pizza": {}, "chicken": {}}},
+                            "frontend": {"junior": {"pizza": {}, "chicken": {}},
+                                         "senior": {"pizza": {}, "chicken": {}}}}}
+
+    answer = [0 for i in range(CONDITIONS)]
+
     for i in range(len(info)):
         info[i] = info[i].split(' ')
-        for j in range(len(info[i])):
-            if j == 0:
-                infoDic['language'].append(info[i][j])
-            elif j == 1:
-                infoDic['category'].append(info[i][j])
-            elif j == 2:
-                infoDic['history'].append(info[i][j])
-            elif j == 3:
-                infoDic['food'].append(info[i][j])
-            else:
-                infoDic['score'].append(info[i][j])
 
-    # query 분리해서 requestList 라는 새로운 리스트생성
-    requestList = [[] for i in range(len(query))]
     for i in range(len(query)):
         query[i] = query[i].split(' ')
+
+    for i in range(len(info)):
+        if info[i][4] not in dictCombi[info[i][0]][info[i][1]][info[i][2]][info[i][3]]:
+            dictCombi[info[i][0]][info[i][1]][info[i][2]][info[i][3]][info[i][4]] = 1
+        else:
+            dictCombi[info[i][0]][info[i][1]][info[i][2]][info[i][3]][info[i][4]] += 1
+
+    splitedQuery = []
+    for i in range(len(query)):
+        splitedQuery.append([])
         for j in range(len(query[i])):
             if query[i][j] != 'and':
-                requestList[i].append(query[i][j])
+                splitedQuery[-1].append(query[i][j])
 
-    requestDic = {'language': [], 'category': [], 'history': [], 'food': [], 'score': []}
-    for i in range(len(requestList)):
-        for j in range(len(requestList[i])):
-            if j == 0:
-                requestDic['language'].append(requestList[i][j])
-            elif j == 1:
-                requestDic['category'].append(requestList[i][j])
-            elif j == 2:
-                requestDic['history'].append(requestList[i][j])
-            elif j == 3:
-                requestDic['food'].append(requestList[i][j])
-            else:
-                requestDic['score'].append(requestList[i][j])
+    def getCorrectApplicant(condition):
+        cnt = 0
+        result = []
+        if condition[0] == '-':
+            condition[0] = ['cpp', 'java', 'python']
+        else:
+            condition[0] = [condition[0]]
+        if condition[1] == '-':
+            condition[1] = ['frontend', 'backend']
+        else:
+            condition[1] = [condition[1]]
+        if condition[2] == '-':
+            condition[2] = ['junior', 'senior']
+        else:
+            condition[2] = [condition[2]]
+        if condition[3] == '-':
+            condition[3] = ['chicken', 'pizza']
+        else:
+            condition[3] = [condition[3]]
+        for lan in condition[0]:
+            for job in condition[1]:
+                for his in condition[2]:
+                    for food in condition[3]:
+                        for sco in dictCombi[lan][job][his][food]:
+                            if int(sco) >= int(condition[4]):
+                                cnt += dictCombi[lan][job][his][food][sco]
+        return cnt
 
-    def validate(i, j):
-        for key in requestDic:
-            if requestDic[key][i] == '-':
-                continue
-            if key == 'score':
-                if int(requestDic[key][i]) <= int(infoDic[key][j]):
-                    continue
-            if requestDic[key][i] != infoDic[key][j]:
-                return
-        result[i] += 1
-        return
+    for i in range(len(splitedQuery)):
+        answer[i] += getCorrectApplicant(splitedQuery[i])
 
-    for i in range(len(query)):
-        for j in range(len(info)):
-            validate(i, j)
-
-    # print(infoDic)
-    # print("\n")
-    # print(requestDic)
-    return result
+    return answer
