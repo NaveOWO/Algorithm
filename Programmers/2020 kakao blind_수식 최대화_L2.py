7a5feb2d766f9c8abfc25d4ec3d0aa116e1dbd92
JY <pianowell96@gmail.com>
2022-10-02 23:13:09 +0900

    수식 최대화

--- Programmers/2020 kakao blind_수식 최대화_L2.py
+++ Programmers/2020 kakao blind_수식 최대화_L2.py
@@ -53,4 +53,4 @@ def solution(expression):
         for operator in currentOrder:
             getParticularResult(operator)
         answer = max(answer, abs(int(originStr[0])))
-    return answer+1
+    return answer
